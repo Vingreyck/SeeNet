eaabd4e2fdb4b1d503226582b81293a3b6c8f5dd
Vinícius Lima Santos <103907631+Vingreyck@users.noreply.github.com>
2025-08-24 17:01:50 -0300

    segregação do back para o front e implementação do js

--- seenet/android/app/src/main/kotlin/com/example/seenet/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.seenet
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- seenet/lib/login/widgets/login.binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:seenet/login/loginview.controller.dart';
-
-class LoginBindings extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => LoginController());
-  }
-}//lazy put vai chamar apenas uma vez a tela de logar

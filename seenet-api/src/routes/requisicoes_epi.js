2f080ec10bd28fa430e83beb23303c2b111f5ad5
Vinícius Lima Santos <103907631+Vingreyck@users.noreply.github.com>
2026-02-27 15:33:48 -0300

    aa

--- seenet-api/src/routes/requisicoes_epi.js
+++ seenet-api/src/routes/requisicoes_epi.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { db } = require('../config/database');
-const { authMiddleware } = require('../middleware/auth');
+const authMiddleware = require('../middleware/auth');
 const PDFDocument = require('pdfkit');
 const https = require('https');
 const http = require('http');

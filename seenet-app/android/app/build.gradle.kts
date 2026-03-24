885ee5de17aa883da0d80d2f28963cca390c432a
Vinícius Lima Santos <103907631+Vingreyck@users.noreply.github.com>
2026-03-24 09:21:23 -0300

    fix: dependencia faltando

--- seenet-app/android/app/build.gradle.kts
+++ seenet-app/android/app/build.gradle.kts
@@ -22,6 +22,7 @@ android {
     ndkVersion = "28.2.13676358"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
@@ -91,5 +92,6 @@ flutter {
 dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("com.google.android.play:integrity:1.5.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 
 }

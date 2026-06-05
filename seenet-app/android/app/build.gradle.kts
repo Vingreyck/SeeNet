56982de89544fab204e467421b46245dc86bdec1
Vinícius Lima Santos <103907631+Vingreyck@users.noreply.github.com>
2026-06-05 10:15:35 -0300

    fix

--- seenet-app/android/app/build.gradle.kts
+++ seenet-app/android/app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         applicationId = "com.seenet.diagnostico"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 52
+        versionCode = 53
         versionName = "1.2"
         multiDexEnabled = true
     }

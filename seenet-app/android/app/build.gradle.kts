c01f5e1f417dcbf502754cb8236f19f586a05add
Vinícius Lima Santos <103907631+Vingreyck@users.noreply.github.com>
2026-07-31 16:42:42 -0300

    bump de versão nova

--- seenet-app/android/app/build.gradle.kts
+++ seenet-app/android/app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.seenet.diagnostico"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 72
-        versionName = "1.6.0"
+        versionCode = 73
+        versionName = "1.6.1"
         multiDexEnabled = true
     }
 
